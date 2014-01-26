3957917d40df98287fd914e4fbee7151627b2507
Pascal Klarenbeek <pascal.klarenbeek@gmail.com>
2014-01-26 22:42:12 +0100

    Fix: nuspec, version number + added clean script

---
+++ Clean.cmd
@@ -0,0 +1,61 @@
+@echo off
+SET Root=%~dp0
+SET TestResult=%Root%TestResults
+SET Sources=%Root%Sources
+SET Tests=%Root%Tests
+SET Package=%Root%Package
+SET Packages=%Root%Packages
+
+IF EXIST %TestResult% (
+echo Removing TestResults from %TestResult%
+rd /S /Q %TestResult%
+) ELSE (
+rem echo No TestResults
+)
+echo Removing bin folders from %Sources%
+for /d /r %Sources% %%d in (bin) do (
+if exist "%%d" (
+echo Removing: %%d
+rd /s/q "%%d"
+)
+)
+
+echo Removing obj folders from %Sources%
+for /d /r %Sources% %%d in (obj) do (
+if exist "%%d" (
+echo Removing: %%d
+rd /s/q "%%d"
+)
+)
+
+echo Removing bin folders from %Tests%
+for /d /r %Tests% %%d in (bin) do (
+if exist "%%d" (
+echo Removing: %%d
+rd /s/q "%%d"
+)
+)
+
+echo Removing obj folders from %Tests%
+for /d /r %Tests% %%d in (obj) do (
+if exist "%%d" (
+echo Removing: %%d
+rd /s/q "%%d"
+)
+)
+
+echo Removing Lib folders from %Package%
+for /d /r %Package% %%d in (Lib) do (
+if exist "%%d" (
+echo Removing: %%d
+rd /s/q "%%d"
+)
+)
+
+echo Removing nupkg files from %Package%
+for /r %Package% %%d in (dir *.nupkg) do (
+if exist "%%d" (
+echo Removing: %%d
+del "%%d"
+)
+)
