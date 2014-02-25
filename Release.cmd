32e058cbc68bf8797cf9d67786c7bdea3d47c5fd
Pascal Klarenbeek <pascal.klarenbeek@gmail.com>
2014-02-25 22:10:49 +0100

    Fixed Release.cmd + PK.NuGet

--- Release.cmd
+++ Release.cmd
@@ -1,5 +1,11 @@
 @echo off
+
+IF NOT DEFINED VS120COMNTOOLS (goto :VSNotDefined)
+
+CALL "%VS120COMNTOOLS%..\..\VC\vcvarsall.bat"
+
 SET Root=%~dp0
+SET PKSolution=%Root%PK.sln
 SET TestResult=%Root%TestResults
 SET Sources=%Root%Sources
 SET Tests=%Root%Tests
@@ -28,6 +34,8 @@ SET PKApplicationProj=%PKApplication%\PK.Application.csproj
 SET PKApplicationWeb=%Sources%\PK.Application.Web.Net45
 SET PKApplicationWebProj=%PKApplicationWeb%\PK.Application.Web.Net45.csproj
 
+%NuGet% restore %PKSolution%
+
 msbuild /t:Pack /p:Configuration=Release %PKNuGetProj%
 msbuild /t:Pack /p:Configuration=Release %PKCommonProj%
 msbuild /t:Pack /p:Configuration=Release %PKSettingsProj%
@@ -35,3 +43,8 @@ msbuild /t:Pack /p:Configuration=Release %PKSettingsStoreAppsProj%
 msbuild /t:Pack /p:Configuration=Release %PKSettingsAppSettingsProj%
 msbuild /t:Pack /p:Configuration=Release %PKApplicationProj%
 msbuild /t:Pack /p:Configuration=Release %PKApplicationWebProj%
+
+goto :eof
+
+:VSNotDefined
+echo Cannot generate packages because VS120COMNTOOLS is not defined
